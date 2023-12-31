9db97e8a3d8e58a9120af0582186144e18990202
proaro2001 <proaro2001@gmail.com>
2023-12-31 02:28:37 -0800

    Successfully move seperate the Welcome Page

--- App.js
+++ App.js
@@ -2,57 +2,8 @@ import React from "react";
 import { ImageBackground, StyleSheet, View, Button, Text } from "react-native";
 import StartBtn from "./app/screens/StartBtn";
 // import language from "./language.json";
-// import Welcome from "./Welcome";
+import Welcome from "./app/screens/Welcome";
 
 export default function App() {
-  return (
-    <View style={styles.imgContainer}>
-      <ImageBackground
-        source={require("./assets/background.jpg")}
-        style={styles.background}
-      >
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>{"Werewolf"}</Text>
-        </View>
-        {/* You can add additional components here that make up your page */}
-        <View style={styles.buttonContainer}>
-          <StartBtn
-            text="Start Game"
-            onPress={() => console.log("Start Game")}
-          />
-        </View>
-      </ImageBackground>
-    </View>
-  );
+  return <Welcome />;
 }
-
-const styles = StyleSheet.create({
-  imgContainer: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  background: {
-    flex: 1,
-    alignItems: "center",
-  },
-  titleContainer: {
-    justifyContent: "flex-start",
-    paddingTop: "25%",
-  },
-  title: {
-    fontSize: 60,
-    fontWeight: "bold",
-    color: "#FFD700",
-    textAlign: "center",
-    fontFamily: "Chalkduster",
-    textShadowColor: "rgba(0, 0, 0, 0.5)",
-    textShadowOffset: { width: 10, height: -5 },
-    textShadowRadius: 5,
-  },
-  buttonContainer: {
-    flex: 1,
-    padding: 100,
-    justifyContent: "flex-end",
-    bottom: 10,
-  },
-});
---
+++ app/screens/Welcome.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Button, Text } from "react-native";
+import StartBtn from "./StartBtn";
+
+function Welcome({}) {
+  return (
+    <View style={styles.imgContainer}>
+      <ImageBackground
+        source={require("../../assets/background.jpg")}
+        style={styles.background}
+      >
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>{"Werewolf"}</Text>
+        </View>
+        {/* You can add additional components here that make up your page */}
+        <View style={styles.buttonContainer}>
+          <StartBtn
+            text="Start Game"
+            onPress={() => console.log("Start Game")}
+          />
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+  imgContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  background: {
+    flex: 1,
+    alignItems: "center",
+  },
+  titleContainer: {
+    justifyContent: "flex-start",
+    paddingTop: "25%",
+  },
+  title: {
+    fontSize: 60,
+    fontWeight: "bold",
+    color: "#FFD700",
+    textAlign: "center",
+    fontFamily: "Chalkduster",
+    textShadowColor: "rgba(0, 0, 0, 0.5)",
+    textShadowOffset: { width: 10, height: -5 },
+    textShadowRadius: 5,
+  },
+  buttonContainer: {
+    flex: 1,
+    padding: 100,
+    justifyContent: "flex-end",
+    bottom: 10,
+  },
+});
